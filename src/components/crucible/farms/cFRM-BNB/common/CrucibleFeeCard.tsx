981b17825e33968bf9974b698c35aad0464a9e0e
Abdul Ahad <abdulahadbinkhalid@gmail.com>
2022-04-01 20:18:00 +0500

    Nits

--- src/components/crucible/farms/cFRM-BNB/common/CrucibleFeeCard.tsx
+++ src/components/crucible/farms/cFRM-BNB/common/CrucibleFeeCard.tsx
@@ -1,4 +1,12 @@
-import { FCard, FTypo, FGrid, FGridItem, FItem, FButton, FContainer } from "ferrum-design-system";
+import {
+  FCard,
+  FTypo,
+  FGrid,
+  FGridItem,
+  FItem,
+  FButton,
+  FContainer,
+} from "ferrum-design-system";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useHistory, useLocation, useParams } from "react-router";
@@ -11,17 +19,26 @@ const CrucibleFeeCard = () => {
   const history = useHistory();
   const location: any = useLocation();
   const { farm } = useParams<{ farm?: string }>();
-  const crucible = useSelector((state: RootState) => state.crucible.selectedCrucible);
+  const crucible = useSelector(
+    (state: RootState) => state.crucible.selectedCrucible
+  );
 
-  const userCrucibleData = useSelector((state: RootState) => state.crucible.userCrucibleDetails);
-  let userStake = (userCrucibleData.stakes || []).find((e: any) => e.address.toLowerCase() === location.state.LPstakingAddress);
+  const userCrucibleData = useSelector(
+    (state: RootState) => state.crucible.userCrucibleDetails
+  );
+  let userStake = (userCrucibleData.stakes || []).find(
+    (e: any) => e.address.toLowerCase() === location.state.LPstakingAddress
+  );
   useEffect(() => {
     console.log("test");
   }, []);
 
   const onClaimRewardsClick = () => {
     history.push({
-      pathname: getActualRoute(farm, PATH_DASHBOARD.crucible.crucibleActionRoutes.withdraw.withdraw),
+      pathname: getActualRoute(
+        farm,
+        PATH_DASHBOARD.crucible.crucibleActionRoutes.withdraw.withdraw
+      ),
     });
   };
 
@@ -33,7 +50,12 @@ const CrucibleFeeCard = () => {
           <FGrid className="info-bar">
             <FGridItem size={[4, 4, 4]}>
               <FItem align={"center"}>
-                <FTypo color="#DAB46E" size={20} weight={700} className="f-pb--2">
+                <FTypo
+                  color="#DAB46E"
+                  size={20}
+                  weight={700}
+                  className="f-pb--2"
+                >
                   {`${BigUtils.safeParse(crucible?.feeOnTransferRate || "0")
                     .times(100)
                     .toString()}%`}
@@ -43,7 +65,12 @@ const CrucibleFeeCard = () => {
             </FGridItem>
             <FGridItem size={[4, 4, 4]}>
               <FItem align={"center"}>
-                <FTypo color="#DAB46E" size={20} weight={700} className="f-pb--2">
+                <FTypo
+                  color="#DAB46E"
+                  size={20}
+                  weight={700}
+                  className="f-pb--2"
+                >
                   {`${BigUtils.safeParse(crucible?.feeOnWithdrawRate || "0")
                     .times(100)
                     .toString()}%`}
@@ -53,7 +80,12 @@ const CrucibleFeeCard = () => {
             </FGridItem>
             <FGridItem size={[4, 4, 4]}>
               <FItem align={"center"}>
-                <FTypo color="#DAB46E" size={20} weight={700} className="f-pb--2">
+                <FTypo
+                  color="#DAB46E"
+                  size={20}
+                  weight={700}
+                  className="f-pb--2"
+                >
                   {crucible?.symbol}
                 </FTypo>
                 <FTypo size={20}>Crucible Token</FTypo>
@@ -63,18 +95,40 @@ const CrucibleFeeCard = () => {
           <FCard className={"styled-card align-v your-crucible"}>
             <FGrid>
               <FGridItem size={[6, 6, 6]} dir="column">
-                <FTypo className="f-pb--2">Your Crucible {farm?.includes("BNB") ? "LP" : ""} Farm Stake</FTypo>
-                <FTypo size={24} weight={600} align={"end"} display="flex" alignY={"end"}>
+                <FTypo className="f-pb--2">
+                  Your Crucible {farm?.includes("BNB") ? "LP" : ""} Farm Stake
+                </FTypo>
+                <FTypo
+                  size={24}
+                  weight={600}
+                  align={"end"}
+                  display="flex"
+                  alignY={"end"}
+                >
                   {Number(userStake?.stakeOf || "0").toFixed(3)}
                   <FTypo size={12} weight={300} className={"f-pl--7 f-pb--1"}>
-                    {farm?.includes("BNB") ? `APE-LP ${crucible?.symbol}-BNB` : crucible?.symbol}
+                    {farm?.includes("BNB")
+                      ? `APE-LP ${crucible?.symbol}-BNB`
+                      : crucible?.symbol}
                   </FTypo>
                 </FTypo>
               </FGridItem>
               <FGridItem size={[6, 6, 6]}>
                 <FItem align="right">
-                  <FTypo color="#DAB46E" size={40} weight={600} align={"end"} display="flex" alignY={"end"}>
-                    <FTypo size={16} weight={500} className={"f-pr--7 f-pb--3"} align="right">
+                  <FTypo
+                    color="#DAB46E"
+                    size={40}
+                    weight={600}
+                    align={"end"}
+                    display="flex"
+                    alignY={"end"}
+                  >
+                    <FTypo
+                      size={16}
+                      weight={500}
+                      className={"f-pr--7 f-pb--3"}
+                      align="right"
+                    >
                       APR
                     </FTypo>
                     192%
@@ -88,11 +142,15 @@ const CrucibleFeeCard = () => {
               <FGridItem size={[6]} dir="column">
                 <FTypo className="f-pb--2">Your unclaimed Rewards</FTypo>
                 <FTypo color="#DAB46E" size={22} weight={700}>
-                  {Number(userStake?.rewardOf || "0").toFixed(3)} {crucible?.symbol}
+                  {Number(userStake?.rewardOf || "0").toFixed(3)}{" "}
+                  {crucible?.symbol}
                 </FTypo>
               </FGridItem>
               <FGridItem size={[6]} alignY="center" alignX={"end"}>
-                <FButton title={"Claim"} onClick={() => onClaimRewardsClick()}></FButton>
+                <FButton
+                  title={"Claim"}
+                  onClick={() => onClaimRewardsClick()}
+                ></FButton>
               </FGridItem>
             </FGrid>
           </FCard>
