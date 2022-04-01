bc136bfced51ad9c3a0f0b051ad605dc13e6a5ac
Abdul Ahad <abdulahadbinkhalid@gmail.com>
2022-04-01 20:21:10 +0500

    Nits

--- src/components/crucible/common/CardMyBalance.tsx
+++ src/components/crucible/common/CardMyBalance.tsx
@@ -4,7 +4,11 @@ import { ReactComponent as IconFerrum } from "../../../assets/img/icon-ferrum.sv
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../redux/rootReducer";
 import * as CrucibleActions from "../redux/CrucibleActions";
-import { getCABNInformation, getTokenInformationFromWeb3, TruncateWithoutRounding } from "../../../utils/global.utils";
+import {
+  getCABNInformation,
+  getTokenInformationFromWeb3,
+  TruncateWithoutRounding,
+} from "../../../utils/global.utils";
 import {
   CBTTokenContractAddress,
   CBTxTokenContractAddress,
@@ -42,11 +46,23 @@ export const CrucibleMyBalance = () => {
         <FListItem>
           <FItem display={"flex"} alignY={"center"} className="w-100">
             <span className="icon-network f-pr--5">
-              <img src={tokenData["CBTToken"]?.logo} height="22px" width="22px" style={{ marginRight: "3px" }} alt="" />
+              <img
+                src={tokenData["CBTToken"]?.logo}
+                height="22px"
+                width="22px"
+                style={{ marginRight: "3px" }}
+                alt=""
+              />
             </span>
             <FTypo>{tokenData["CBTToken"]?.symbol}</FTypo>
           </FItem>
-          <FTypo size={30} weight={600} align={"end"} display="flex" alignY={"end"}>
+          <FTypo
+            size={30}
+            weight={600}
+            align={"end"}
+            display="flex"
+            alignY={"end"}
+          >
             {TruncateWithoutRounding(tokenData["CBTToken"]?.balance, 3) || 0}
           </FTypo>
         </FListItem>
@@ -64,8 +80,15 @@ export const CrucibleMyBalance = () => {
             </span>
             <FTypo>{tokenData["APELPCFRMBNB"]?.symbol}</FTypo>
           </FItem>
-          <FTypo size={30} weight={600} align={"end"} display="flex" alignY={"end"}>
-            {TruncateWithoutRounding(tokenData["APELPCFRMBNB"]?.balance, 3) || 0}
+          <FTypo
+            size={30}
+            weight={600}
+            align={"end"}
+            display="flex"
+            alignY={"end"}
+          >
+            {TruncateWithoutRounding(tokenData["APELPCFRMBNB"]?.balance, 3) ||
+              0}
           </FTypo>
         </FListItem>
 
@@ -82,7 +105,13 @@ export const CrucibleMyBalance = () => {
             </span>
             <FTypo>{tokenData["CBTxToken"]?.symbol}</FTypo>
           </FItem>
-          <FTypo size={30} weight={600} align={"end"} display="flex" alignY={"end"}>
+          <FTypo
+            size={30}
+            weight={600}
+            align={"end"}
+            display="flex"
+            alignY={"end"}
+          >
             {TruncateWithoutRounding(tokenData["CBTxToken"]?.balance, 3) || 0}
           </FTypo>
         </FListItem>
@@ -100,8 +129,15 @@ export const CrucibleMyBalance = () => {
             </span>
             <FTypo>{tokenData["APELPCFRMxBNB"]?.symbol}</FTypo>
           </FItem>
-          <FTypo size={30} weight={600} align={"end"} display="flex" alignY={"end"}>
-            {TruncateWithoutRounding(tokenData["APELPCFRMxBNB"]?.balance, 3) || 0}
+          <FTypo
+            size={30}
+            weight={600}
+            align={"end"}
+            display="flex"
+            alignY={"end"}
+          >
+            {TruncateWithoutRounding(tokenData["APELPCFRMxBNB"]?.balance, 3) ||
+              0}
           </FTypo>
         </FListItem>
       </FList>
