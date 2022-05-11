b2dd395f30788fd2e040f4f516b3c8f27f64942d
Maryam Arif <mariamarif998@gmail.com>
2022-05-11 12:52:07 +0500

    exponential change

--- src/utils/global.utils.js
+++ src/utils/global.utils.js
@@ -218,3 +218,14 @@ export function changeExponentToPoints(x) {
   console.log("end", x);
   return x;
 }
+
+export function convertExponentialToDecimal(exponentialNumber) {
+  const str = exponentialNumber.toString();
+  if (str.indexOf("e") !== -1) {
+    const exponent = parseInt(str.split("-")[1], 10);
+    const result = exponentialNumber.toFixed(exponent);
+    return result;
+  } else {
+    return exponentialNumber;
+  }
+}
