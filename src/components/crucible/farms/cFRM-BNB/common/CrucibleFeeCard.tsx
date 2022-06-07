4718b074b0f6cf3e3c65b9735e71b964cdbfc551
Maryam Arif <mariamarif998@gmail.com>
2022-06-07 19:53:25 +0500

    fixed undefined on load issue in apr after fetch

--- src/components/crucible/farms/cFRM-BNB/common/CrucibleFeeCard.tsx
+++ src/components/crucible/farms/cFRM-BNB/common/CrucibleFeeCard.tsx
@@ -108,8 +108,8 @@ const CrucibleFeeCard = () => {
                       style={{ height: 9, width: 10, marginRight: 22 }}
                     />
                     <div>
-                      <p className="medium-text-400 text-center">{aprData[activeAPRIndex].label}</p>
-                      <p className="text-35 text-center default-text-color">{aprData[activeAPRIndex].value}</p>
+                      <p className="medium-text-400 text-center">{aprData[activeAPRIndex]?.label}</p>
+                      <p className="text-35 text-center default-text-color">{aprData[activeAPRIndex]?.value}</p>
                     </div>
                     <img
                       className="cursor-pointer"
