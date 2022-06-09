f803a6dea55276c8caac0b0e80230cc9211d51b1
Maryam Arif <mariamarif998@gmail.com>
2022-06-09 21:00:37 +0500

    implemented chnages required to deploy new staking leaderboard and competitions

--- src/components/leaderboard/LeaderboardHelper.js
+++ src/components/leaderboard/LeaderboardHelper.js
@@ -1,25 +1,27 @@
 export const chainIdList = [
-    { id: '1', label: 'Ethereum' },
-    { id: '4', label: 'Rinkeby' },
-    { id: '56', label: 'BSC' },
-    { id: '97', label: 'BSC Testnet' },
-    { id: '137', label: 'Polygon (Matic)' },
-    { id: '80001', label: 'Polygon Testnet (Mumbai) ' },
-    { id: '128', label: 'HECO' },
-    { id: '256', label: 'HECO Testnet' }
-  ];
+  { id: "1", label: "Ethereum" },
+  { id: "4", label: "Rinkeby" },
+  { id: "56", label: "BSC" },
+  { id: "97", label: "BSC Testnet" },
+  { id: "137", label: "Polygon (Matic)" },
+  { id: "80001", label: "Polygon Testnet (Mumbai) " },
+  { id: "128", label: "HECO" },
+  { id: "256", label: "HECO Testnet" },
+];
 
-  export const dexUrlList = [
-    { url: 'https://app.uniswap.org/#/', label: 'https://app.uniswap.org/#/' },
-    { url: 'https://pancakeswap.finance/', label: 'https://pancakeswap.finance/' },
-    { url: 'https://app.apeswap.finance/', label: 'https://app.apeswap.finance/' },
-    { url: 'https://exchange.dfyn.network/#/', label: 'https://exchange.dfyn.network/#/' },
-    { url: 'https://quickswap.exchange/#/', label: 'https://quickswap.exchange/#/' }
-  ];
+export const dexUrlList = [
+  { url: "https://app.uniswap.org/#/", label: "https://app.uniswap.org/#/" },
+  { url: "https://pancakeswap.finance/", label: "https://pancakeswap.finance/" },
+  { url: "https://app.apeswap.finance/", label: "https://app.apeswap.finance/" },
+  { url: "https://exchange.dfyn.network/#/", label: "https://exchange.dfyn.network/#/" },
+  { url: "https://quickswap.exchange/#/", label: "https://quickswap.exchange/#/" },
+];
 
-  export const filterList = (items, addressList) => {  
-    if (items && items.length > 0 && addressList && addressList.length > 0) { 
-      const filteredList =  items.filter((element2) => addressList.map((element1) => element1).indexOf(element2.tokenHolderAddress) === -1);  
-      return filteredList;
-    }  
-  };
+export const filterList = (items, addressList) => {
+  if (items && items.length > 0 && addressList && addressList.length > 0) {
+    const filteredList = items.filter(
+      (element2) => addressList.map((element1) => element1).indexOf(element2.tokenHolderAddress || element2.TokenHolderAddress || element2.address) === -1
+    );
+    return filteredList;
+  }
+};
