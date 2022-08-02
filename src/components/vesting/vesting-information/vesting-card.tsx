ef5152e6caad01f4968342b360f49da6eda054b0
faizan-31 <72381634+faizan-31@users.noreply.github.com>
2022-08-02 17:45:23 +0500

    admin portal vesting cards

--- src/components/vesting/vesting-information/vesting-card.tsx
+++ src/components/vesting/vesting-information/vesting-card.tsx
@@ -6,43 +6,49 @@ const VestingCards = () => {
         <>
             <FGrid className={'f-mt-2 f-mb-2'}>
                 <FGridItem size={[4, 4, 4]}>
-                    <FCard variant={'whiteLabeled'} height={'170px'} >
-                        <FTypo size={20} weight={400} color="white" className={"f-pb-1"}>
-                            Total Vesting
-                        </FTypo>
-                        <FTypo size={25} weight={400} color="#DAB46E" align={"end"} display="flex" alignY={"end"} className="f-mt--5">
-                            123456.789
-                            <FTypo size={14} weight={700} color="white" className={"f-pl--7 f-pb--1"}>
-                                TOKEN
+                    <div className={'small-vesting-card'}>
+                        <div className='f-ml-1'>
+                            <FTypo size={20} weight={400} color="white" className={"f-pb-1"}>
+                                Total Vesting
                             </FTypo>
-                        </FTypo>
-                    </FCard>
+                            <FTypo size={25} weight={400} color="#DAB46E" align={"end"} display="flex" alignY={"end"} className="f-mt--5">
+                                123456.789
+                                <FTypo size={14} weight={700} color="white" className={"f-pl--7 f-pb--1"}>
+                                    TOKEN
+                                </FTypo>
+                            </FTypo>
+                        </div>
+                    </div>
                 </FGridItem>
                 <FGridItem size={[4, 4, 4]}>
-                    <FCard variant={'whiteLabeled'} height={'170px'}>
-                        <FTypo size={20} weight={400} color="white" className={"f-pb-1"}>
-                            Total Claimable
-                        </FTypo>
-                        <FTypo size={25} weight={400} color="#DAB46E" align={"end"} display="flex" alignY={"end"} className="f-mt--5">
-                            123456.789
-                            <FTypo size={14} weight={700} color="white" className={"f-pl--7 f-pb--1"}>
-                                TOKEN
+                    <div className={'small-vesting-card'}>
+                        <div className='f-ml-1'>
+                            <FTypo size={20} weight={400} color="white" className={"f-pb-1"}>
+                                Total Claimable
                             </FTypo>
-                        </FTypo>
-                    </FCard>
+                            <FTypo size={25} weight={400} color="#DAB46E" align={"end"} display="flex" alignY={"end"} className="f-mt--5">
+                                123456.789
+                                <FTypo size={14} weight={700} color="white" className={"f-pl--7 f-pb--1"}>
+                                    TOKEN
+                                </FTypo>
+                            </FTypo>
+                        </div>
+                    </div>
                 </FGridItem>
                 <FGridItem size={[4, 4, 4]}>
-                    <FCard variant={'whiteLabeled'} height={'170px'}>
-                        <FTypo size={20} weight={400} color="white" className={"f-pb-1"}>
-                            Total Claimed
-                        </FTypo>
-                        <FTypo size={25} weight={400} color="#DAB46E" align={"end"} display="flex" alignY={"end"} className="f-mt--5">
-                            123456.789
-                            <FTypo size={14} weight={700} color="white" className={"f-pl--7 f-pb--1"}>
-                                TOKEN
+                    <div className={'small-vesting-card f-pl-1'}>
+                        <div className='f-ml-1'>
+                            <FTypo size={20} weight={400} color="white" className={"f-pb-1"}>
+                                Total Claimed
                             </FTypo>
-                        </FTypo>
-                    </FCard>
+                            <FTypo size={25} weight={400} color="#DAB46E" align={"end"} display="flex" alignY={"end"} className="f-mt--5">
+                                123456.789
+                                <FTypo size={14} weight={700} color="white" className={"f-pl--7 f-pb--1"}>
+                                    TOKEN
+                                </FTypo>
+                            </FTypo>
+                        </div>
+                    </div>
                 </FGridItem>
             </FGrid>
         </>
