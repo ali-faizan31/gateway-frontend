7c45e853aea8e5592494fdc6ff6732c15769d430
faizan-31 <72381634+faizan-31@users.noreply.github.com>
2022-08-25 18:50:40 +0500

    popup cards for all flows

--- src/components/crucible/farms/cFRM-BNB/UnWrapFlow/DialogTransitionStatus.tsx
+++ src/components/crucible/farms/cFRM-BNB/UnWrapFlow/DialogTransitionStatus.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 // import { useHistory } from "react-router";
-import { FButton, FDialog, FItem, FLoader, FTypo } from "ferrum-design-system";
-import { ReactComponent as IconApprove } from "../../../../../assets/img/icon-transaction-approved.svg";
-import { ReactComponent as IconSubmitted } from "../../../../../assets/img/icon-transaction-submitted.svg";
-import Loader from "../../../../../assets/gif/loader.svg";
+import { FButton, FItem, FLoader, FTypo } from "ferrum-design-system";
+import { ReactComponent as IconApprove } from "../../../../../assets/img/new-transaction-approved.svg";
+import IconSubmitted from "../../../../../assets/img/icon-transaction-submitted.svg";
+import underLine from "../../../../../assets/img/lines-in-end.svg"
+import Loader from "../../../../../assets/gif/wallet-transaction-loader.gif";
 // import { PATH_DASHBOARD } from "../../../../../routes/paths";
 import { linkForTransaction, addToken } from "./../../../../../container-components/web3Client/types";
+import { FDialog } from "../../../../vesting/ferrum-design-system/Fdialog/Fdialog";
 
 export const DialogTransitionStatus = ({
   transitionStatusDialog,
@@ -22,47 +24,53 @@ export const DialogTransitionStatus = ({
   };
 
   return (
-    <FDialog size={"medium"} show={transitionStatusDialog} onHide={() => onclose()} className="transaction-status text-center" showClose={false}>
+    <FDialog size={"medium"} variant={"new-popup"} show={transitionStatusDialog} onHide={() => onclose()} className="transaction-status text-center" showClose={true}>
       {isProcessing ? (
         <React.Fragment>
           <FItem align="center">
-            <FLoader loading={true} width={100} loaderImage={Loader} />
-            <FTypo size={20}>Confirm This Transaction in your Wallet.</FTypo>
+            <img src={Loader} alt={''} style={{ width: 100, height: 100 }} />
+            <FTypo size={20} weight={700} className="f-mt-1 f-pb-3">Confirm This Transaction in your Wallet.</FTypo>
           </FItem>
         </React.Fragment>
       ) : isSubmitted === true ? (
         <React.Fragment>
           <FItem align="center">
-            <IconSubmitted />
-            <FTypo color="#DAB46E" size={22} weight={600} className="f-mt-1">
+            <img src={IconSubmitted} style={{ width: 100, height: 100 }} alt="" />
+
+            <FTypo color="#FFFFFF" size={20} weight={700} className="f-mt-1">
               Transaction Submitted
             </FTypo>
-            <FTypo size={20} weight={600} className="f-mt-1">
+            <FTypo size={14} weight={700} className="f-mt-1">
               View on Explorer
             </FTypo>
-            <FButton title={"Add Token to Metamask"} outlined variant={"secondary"} className="f-mt-1 f-mb-1" />
-            <FItem bgColor="#1D232B" align={"center"} className="f-pt--5 f-pb--5 f-pl-3 f-pr-3" display={"inline-block"}>
-              <FTypo size={16} weight={500}>
-                Tx Processing - Please Wait
-              </FTypo>
-            </FItem>
+            <FButton
+              title={"ADD TOKEN TO METAMASK"}
+              outlined
+              variant={"secondary"}
+              className="f-mt-2 f-mb-1 custom-font-size-16 font-700 primary-border btn-44-h"
+            />
+            <FTypo size={12} weight={700} color={'#D9B373'} className={'f-mt--5'}>
+              Tx Processing - Please Wait
+            </FTypo>
+            <img src={underLine} alt="" className={'f-mb-3 f-mt-1'} />
           </FItem>
         </React.Fragment>
       ) : isProcessed === true ? (
         <React.Fragment>
           <FItem align="center">
-            <IconSubmitted />
-            <FTypo color="#DAB46E" size={22} weight={600} className="f-mt-1">
-              Transaction Processed
+            <img src={IconSubmitted} style={{ width: 100, height: 100 }} alt="" />
+            <FTypo color="#FFFFFF" size={20} weight={700} className="f-mt-1">
+              Transaction Submitted
             </FTypo>
-            <FTypo size={20} weight={600} className="f-mt-1 cursor-pointer">
+            <FTypo size={14} weight={700} className="f-mt-1 cursor-pointer">
               <span onClick={() => window.open(linkForTransaction(crucible.network, transactionId), "_blank")}>View on Explorer</span>
             </FTypo>
             <FItem>
               <FButton
-                title={"Add Token to Metamask"}
+                title={"ADD TOKEN TO METAMASK"}
                 outlined
                 variant={"secondary"}
+                className="f-mt-2 f-mb-1 custom-font-size-16 font-700 primary-border btn-44-h"
                 onClick={() =>
                   addToken({
                     currency: crucible?.contractAddress,
@@ -72,11 +80,13 @@ export const DialogTransitionStatus = ({
                     logoURI: "",
                   })
                 }
-                className="f-mt-1 f-mb-1"
               />
             </FItem>
             <FItem>
-              <FButton title={"Continue To Next Step"} className="btn-step f-mt-1 f-mb-1" onClick={() => onContinueToNextStepClick()} />
+              <FButton
+                title={"CONTINUE TO NEXT STEP"}
+                className="btn-step f-mt-1 f-mb-3 custom-font-size-16 font-700 btn-44-h clr_black_new"
+                onClick={() => onContinueToNextStepClick()} />
             </FItem>
           </FItem>
         </React.Fragment>
